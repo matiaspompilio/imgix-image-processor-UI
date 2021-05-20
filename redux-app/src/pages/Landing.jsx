b9d712277a35158482d4727709c051a2596bbc01
matiaspompilio <matiaspompilio97@gmail.com>
2021-05-20 16:24:02 -0300

    removing git submodule

---
+++ redux-app/src/pages/Landing.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Cracked from 'app/components/Animation/Cracked';
+
+import './Landing.css';
+
+const Landing = () => <Cracked>redux-app</Cracked>;
+
+export default Landing;
